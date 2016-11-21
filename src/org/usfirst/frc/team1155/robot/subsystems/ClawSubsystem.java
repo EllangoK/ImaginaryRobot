6955b74fd6b8186ef17ce448e2dc8ca9a8c8577a
P250-SandDune <lib@bxscience.edu>
2016-11-20 20:23:27 -0500

    Added a claw subsystem

---
+++ src/org/usfirst/frc/team1155/robot/subsystems/ClawSubsystem.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team1155.robot.subsystems;
+import org.usfirst.frc.team1155.robot.Hardware;
+import org.usfirst.frc.team1155.robot.Robot;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+public class ClawSubsystem extends Subsystem {
+  
+  private CANTalon lefTal, righTal;
+ 
+  public ClawSubsystem() {
+    lefTal = Hardware.INSTANCE.leftTalon;
+    righTal = Hardware.INSTANCE.rightTalon;
+
+    lefTal.changeControlMode(CANTalon.TalonControlMode.Follower);
+    lefTal.set(righTal.getDeviceID());
+  }
+  
+  public void lift(boolean pushed) {
+    if(pushed){
+      righTal.set(1);
+    }
+  }
+  
+  public void lower(boolean pressed){
+    if(pressed){
+      righTal.set(-1)
+    }
+  }
+  
+  public void stopPls() {
+    righTal.set(0)
+  }
+}

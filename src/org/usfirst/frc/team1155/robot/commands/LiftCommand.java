1b7a8eaf6b3580c8b112d009da0469163cfd9f24
P250-SandDune <lib@bxscience.edu>
2016-11-20 20:24:23 -0500

    Added commands to lift and lower claw

---
+++ src/org/usfirst/frc/team1155/robot/commands/LiftCommand.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team1155.robot.commands;
+import org.usfirst.frc.team1155.robot.Hardware;
+import org.usfirst.frc.team1155.robot.Robot;
+import org.usfirst.frc.team1155.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.buttons
+
+
+public class LiftCommand extends Command {
+  private static InternalButton pushyDevice;
+  
+  private ClawSubsystem brodoyouevenlift = Robot.lift;
+  
+  public LiftCommand() {
+    requires(Robot.lift);
+    
+    pushyDevice = Hardware.INSTANCE.liftButton;
+  }
+  
+  @Override
+  protected void initialize() {
+  }
+  
+  @Override
+  protected void execute() {
+    brodoyouevenlift.lift(pushyDevice.get());
+  }
+  
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+  
+  @Override
+  protected void end() {
+    brodoyouevenlift.stopPls();
+  }
+  
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
---
+++ src/org/usfirst/frc/team1155/robot/commands/LowerCommand.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team1155.robot.commands;
+import org.usfirst.frc.team1155.robot.Hardware;
+import org.usfirst.frc.team1155.robot.Robot;
+import org.usfirst.frc.team1155.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.buttons
+
+
+public class LowerCommand extends Command {
+  private static InternalButton pressyMachine;
+  
+  private ClawSubsystem nahIlower = Robot.lower;
+  
+  public LowerCommand() {
+    requires(Robot.lower);
+    
+    pressyMachine = Hardware.INSTANCE.lowerButton;
+  }
+  
+  @Override
+  protected void initialize() {
+  }
+  
+  @Override
+  protected void execute() {
+    nahIlower.lower(pressyMachine.get());
+  }
+  
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+  
+  @Override
+  protected void end() {
+    nahIlower.stopPls();
+  }
+  
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}

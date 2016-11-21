d8aad0da5159cd896c75003169a124790a525110
EllangoK <ellangok@bxscience.edu>
2016-11-21 16:02:41 -0500

    Created the Actual Hardware

--- src/org/usfirst/frc/team1155/robot/Hardware.java
+++ src/org/usfirst/frc/team1155/robot/Hardware.java
@@ -10,4 +10,18 @@ public enum Hardware {
 	public CANTalon frontRightTalon, backRightTalon, frontLeftTalon, backLeftTalon;
 	public CANTalon lefTal, righTal;
 	public InternalButton lowerButton, liftButton;
+	
+	Hardware(){
+	gamepad = new Joystick(0);
+	leftJoystick = new Joystick(1);
+	rightJoystick = new Joystick(2);
+	frontRightTalon = new CANTalon(0);
+	backRightTalon = new CANTalon(2);
+	frontLeftTalon = new CANTalon(3);
+	backLeftTalon = new CANTalon(5);
+	lefTal = new CANTalon(10);
+	righTal = new CANTalon(11);
+	lowerButton = new InternalButton(1)
+	liftButton = new InternalButton(2)
+	}
 }
